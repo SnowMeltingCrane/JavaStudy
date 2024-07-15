96a50b61f1d2f902e108f43dcfad252013c99a31
SnowCrane <1277456806@qq.com>
2024-07-16 02:23:59 +0800

    接口的继承

---
+++ src/com/test/entity/Test.java
@@ -0,0 +1,15 @@
+package com.test.entity;
+
+import java.io.Serializable;
+
+public class Test implements A{
+    @Override
+    public void hello() {
+
+    }
+
+    @Override
+    public void test() {
+
+    }
+}
