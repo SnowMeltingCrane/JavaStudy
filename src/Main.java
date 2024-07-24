ddd661e092f5b89dee58e95e15843b91bbca1367
SnowCrane <1277456806@qq.com>
2024-07-25 03:45:35 +0800

    二分查找

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,22 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         int[] arr = new int[]{5,6,4,2,5,8,5,6,2,3};
-        Main.sort(arr);
+        Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
+        System.out.println(Main.search(arr, 7));
     }
 
-    public static void sort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            boolean flag = false;
-            for (int j = 0; j < arr.length-i-1; j++) {
-                if (arr[j] > arr[j+1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                    flag = true;
-                }
-            }
-            if(!flag){
-                break;
-            }
-        }
+    public static int search(int[] arr,int a) {
+       int left = 0,right = arr.length-1;
+       while(left <= right) {
+           int mid = (left+right)/2;//会变的东西要写在循环体内
+           if(arr[mid] < a)
+               left = mid+1;
+           else if(arr[mid] > a)
+               right = mid-1;
+           else
+               return mid;
+       }
+       return -1;
     }
 }
