63f865477c604c4e32b7ee5547fd7f62456fbef0
SnowCrane <1277456806@qq.com>
2024-07-25 02:16:18 +0800

    异常的处理

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 import com.test.entity.*;
 
 public class Main {
-    public static void main(String[] args) throws CloneNotSupportedException {
-        Main main = new Main();
-        Study study = main::lbwnb;
-        System.out.println(study.study());
+    public static void main(String[] args) {
+        try{
+            System.out.println("我是try");
+            int[] arr={1,2,3,4,5};
+            arr[-1]= 10;
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("我是catch");
+        }finally {
+            System.out.println("我是finally");
+        }
+
     }
 
-    public String lbwnb(){
-        return  "卡布奇诺今犹在，不见当年倒茶人";
+    private static int test(int a ,int b){
+        if(b==0)
+            throw new RuntimeException("除数不能为0");
+        return a / b;
     }
 }
