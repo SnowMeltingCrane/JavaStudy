e298fa5a9901747ac472a1fdb904a1793e59cb5c
SnowCrane <1277456806@qq.com>
2024-07-26 02:22:38 +0800

    递归

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,23 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(test(10));
+        int[] arr = {1,5,9,10,46,98};
+        System.out.println(binarySearch(arr,0,arr.length-1,46));
     }
     public static int test(int n){
-        int[] arr = new int[n+1];
-        arr[0] = 1;
-        arr[1] = 1;
-        for(int i=2; i<=n; i++){
-            arr[i] = arr[i-1] + arr[i-2];
-        }
-        return arr[n];
+        if(n==0||n==1)
+            return 1;
+        return test(n-1) + test(n-2);
+    }
+
+    private static int binarySearch(int[] arr, int left ,int right,int target){
+        if(left>right) return -1;
+        int mid = (left + right)/2;
+        if(arr[mid]>target)
+            return binarySearch(arr,left,mid-1,target);
+        else if(arr[mid]<target)
+            return binarySearch(arr,mid+1,right,target);
+        else
+            return mid;
     }
 }
