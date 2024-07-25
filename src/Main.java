4b210cc011c3b05c49629eee90524dda7396ddac
SnowCrane <1277456806@qq.com>
2024-07-26 02:34:58 +0800

    回文串

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,23 @@ import com.test.entity.*;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1,5,9,10,46,98};
-        System.out.println(binarySearch(arr,0,arr.length-1,46));
+        Scanner scanner = new Scanner(System.in);
+        char[] arr=scanner.next().toCharArray();
+        System.out.println(test(arr));
     }
-    public static int test(int n){
-        if(n==0||n==1)
-            return 1;
-        return test(n-1) + test(n-2);
+    public static boolean test(char[] arr){
+        int i=0,j= arr.length-1;
+        while(i<j){
+            if(arr[i]!=arr[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
     }
 
-    private static int binarySearch(int[] arr, int left ,int right,int target){
-        if(left>right) return -1;
-        int mid = (left + right)/2;
-        if(arr[mid]>target)
-            return binarySearch(arr,left,mid-1,target);
-        else if(arr[mid]<target)
-            return binarySearch(arr,mid+1,right,target);
-        else
-            return mid;
-    }
 }
