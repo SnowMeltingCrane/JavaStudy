0c2c0ae20b8568e1761246edf1038ea56b31cfc8
SnowCrane <1277456806@qq.com>
2024-07-29 02:46:24 +0800

    泛型方法

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 import com.test.entity.*;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Score<String> score = new Score<>("计算机网络实验","100","优秀");
-        System.out.println(score);
+        Integer[] arr = {1,4,2,5,3,7,6,9,8};
+        Arrays.sort(arr, (o1, o2) -> o2-o1);
+        System.out.println(Arrays.toString(arr));
+
+        String t= Main.test("test");
+        System.out.println(t);
     }
+
+    public static <T> T test(T t){
+        return t;
+    }
+
 }
---
+++ src/com/test/entity/A.java
@@ -0,0 +1,7 @@
+package com.test.entity;
+
+public class A<T> implements Test<T>{
+    public T test() {
+        return null;
+    }
+}
