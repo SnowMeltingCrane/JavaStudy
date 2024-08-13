0c9a17b755c500c4a06b4c71f725ae3b6834023c
SnowCrane <1277456806@qq.com>
2024-08-13 22:51:24 +0800

    文件复制进度条

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        try(FileReader in = new FileReader("text.txt");
-            FileWriter out = new FileWriter("xxx.txt")) {
+        File file = new File("火男.mp4");
+        try(FileInputStream in = new FileInputStream(file);
+            FileOutputStream out = new FileOutputStream("xxx.mp4")){
             int len;
-            char[] buf = new char[3];
-            while((len = in.read(buf))!=-1){
+            byte[] buf = new byte[1024];
+            long total = file.length(),sum = 0;
+            while ((len = in.read(buf)) != -1) {
                 out.write(buf, 0, len);
+                sum += len;
+                System.out.println("文件已拷贝"+(sum * 100 / total)+ "%");
             }
         }catch(IOException e){
             e.printStackTrace();
