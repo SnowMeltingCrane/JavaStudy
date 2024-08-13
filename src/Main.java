b815ff92fd13afe7c29a26cc7f2e17066b959424
SnowCrane <1277456806@qq.com>
2024-08-14 01:43:20 +0800

    对象流

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 
+import com.test.entity.Student;
 
 import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("火男.mp4");
-        try(FileInputStream in = new FileInputStream(file);
-            FileOutputStream out = new FileOutputStream("xxx.mp4")){
-            int len;
-            byte[] buf = new byte[1024];
-            long total = file.length(),sum = 0;
-            while ((len = in.read(buf)) != -1) {
-                out.write(buf, 0, len);
-                sum += len;
-                System.out.println("文件已拷贝"+(sum * 100 / total)+ "%");
-            }
+        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("text.txt"));
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream("text.txt"))){
+            Student student = new Student();
+            student.name="小明";
+            student.age=18;
+            out.writeObject(student);
+            System.out.println(in.readObject());
+
         }catch(IOException e){
             e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/com/test/entity/Student.java
+++ src/com/test/entity/Student.java
@@ -1,6 +1,20 @@
 package com.test.entity;
 
-public class Student {
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    public String name;
+
+    transient public int age;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
     public void hello(){
         System.out.println("我是学生");
     }
