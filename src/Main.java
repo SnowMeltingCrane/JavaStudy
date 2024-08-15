d894bdac25790e2bcb9c1cecbf590aea2b50d351
SnowCrane <1277456806@qq.com>
2024-08-16 03:45:39 +0800

    线程的礼让

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,30 @@
 import com.test.entity.Student;
 
 import java.io.*;
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("text.txt"));
-            ObjectInputStream in = new ObjectInputStream(new FileInputStream("text.txt"))){
-            Student student = new Student();
-            student.name="小明";
-            student.age=18;
-            out.writeObject(student);
-            System.out.println(in.readObject());
-
-        }catch(IOException e){
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(()-> {
+            System.out.println("线程1开始运行");
+            for (int i = 0; i < 50; i++) {
+                if(i%5==0){
+                    System.out.println("让位");
+                    Thread.yield();
+                }
+                System.out.println("线程1打印:"+i);
+            }
+            System.out.println("线程1结束");
+        });
+        Thread t2 = new Thread(()-> {
+            System.out.println("线程2开始运行");
+            for (int i = 0; i < 50; i++) {
+                System.out.println("线程2打印:"+i);
+            }
+            System.out.println("线程2结束");
+        });
+        t1.start();
+        t2.start();
     }
 }
