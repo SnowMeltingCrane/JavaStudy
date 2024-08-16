86724a8792b4c7a30b2853d7bed17c2910de1491
SnowCrane <1277456806@qq.com>
2024-08-17 01:43:52 +0800

    生产者和消费者

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 
 import com.test.entity.Student;
+import jdk.internal.org.objectweb.asm.tree.IntInsnNode;
 
 import java.io.*;
 import java.sql.SQLOutput;
@@ -7,36 +8,47 @@ import java.util.*;
 
 public class Main {
 
+    public static final Queue<Object> queue = new LinkedList<>();
 
+    public static void main(String[] args)  {
+        new Thread(Main::cook,"蔡徐坤").start();
+        new Thread(Main::cook,"卢本伟").start();
+        new Thread(Main::eat,"hanghang").start();
+        new Thread(Main::eat,"卑劣的凡").start();
+        new Thread(Main::eat,"唐启华").start();
+    }
 
-    public static void main(String[] args) throws InterruptedException {
-        Object lock1 = new Object();
-        Object lock2 = new Object();
-        new Thread(()->{
-            synchronized (lock1){
-                try {
-                    System.out.println("线程1运行中");
-                    Thread.sleep(1000);
-                    System.out.println("开始等待");
-                    lock1.wait();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+    private static void cook(){
+        while(true){
+            try {
+                Thread.sleep(3000);
+                synchronized (queue) {
+                    queue.offer(new Object());
+                    String name = Thread.currentThread().getName();
+                    System.out.println("厨师:"+name+"在"+new Date()+"制作了一份菜品");
+                    queue.notifyAll();
                 }
-                System.out.println("线程1运行结束");
+            }catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        }).start();
+        }
+    }
 
-        new Thread(()->{
-            synchronized (lock1){
-                System.out.println("线程2运行中");
-                try {
-                    Thread.sleep(1000);
-                    lock1.notify();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+    private static void eat(){
+        while(true){
+            try{
+                synchronized (queue) {
+                    while(queue.isEmpty()){
+                        queue.wait();
+                    }
+                    queue.poll();
+                    String name = Thread.currentThread().getName();
+                    System.out.println("顾客:"+name+"在"+new Date()+"开始享用一道菜");
                 }
-                System.out.println("线程2运行结束");
+                Thread.sleep(4000);
+            }catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-        }).start();
+        }
     }
 }
